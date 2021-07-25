09f41cb217702bd017d368144076de9f6698ef23
Kingmidas74 <kingmidas1992@gmail.com>
2021-07-25 11:04:05 +0300

    add Hex terrain

---
+++ src/core/terrains/HexTerrain.go
@@ -0,0 +1,53 @@
+package terrains
+
+import (
+	"github.com/Kingmidas74/gonesis/contracts"
+	"github.com/Kingmidas74/gonesis/utils"
+)
+
+type HexTerrain struct {
+	Terrain
+}
+
+func (this *HexTerrain) GetNeighbor(x, y int, direction int) contracts.ICell {
+	direction = utils.ModLikePython(direction, 6)
+	targetX := x
+	targetY := y
+
+	switch direction {
+	case 0:
+		targetX -= 1
+		targetY -= 1
+	case 1:
+		targetY -= 1
+	case 2:
+		targetX -= 1
+	case 3:
+		targetX += 1
+	case 4:
+		targetX -= 1
+		targetY += 1
+	case 5:
+		targetY += 1
+	}
+	return this.GetCell(targetX, targetY)
+}
+
+func (this *HexTerrain) GetNeighbors(x, y int) []contracts.ICell {
+	result := make([]contracts.ICell, 0)
+
+	coords := []int{
+		x - 1, y - 1,
+		x, y - 1,
+
+		x - 1, y,
+		x + 1, y,
+
+		x - 1, y + 1,
+		x, y + 1,
+	}
+	for i := 0; i < len(coords); i = i + 2 {
+		result = append(result, this.GetCell(coords[i], coords[i+1]))
+	}
+	return result
+}

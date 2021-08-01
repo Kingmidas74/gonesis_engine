002c24d79639cc6e1af281690e038004744f92a1
Kingmidas74 <kingmidas1992@gmail.com>
2021-08-01 21:01:27 +0300

    Add simple benchmark

--- src/World_test.go
+++ src/World_test.go
@@ -15,325 +15,152 @@ import (
 	"time"
 )
 
+func GetCommands() map[int]contracts.ICommand {
+	commandsMap := make(map[int]contracts.ICommand)
+	commandsMap[0] = &preparedCommands.MoveCommand{
+		commands.Command{
+			IsInterrupt: true,
+		},
+	}
+	commandsMap[1] = &preparedCommands.EatCommand{
+		commands.Command{
+			IsInterrupt: false,
+		},
+	}
+	return commandsMap
+}
+
+func GetTerrain() contracts.ITerrain {
+	terrain := terrains.MooreTerrain{
+		terrains.Terrain{
+			Cells:  make([]contracts.ICell, 0),
+			Width:  10,
+			Height: 5,
+		},
+	}
+
+	for y := 0; y < terrain.Height; y++ {
+		for x := 0; x < terrain.Width; x++ {
+			currentCell := terrains.Cell{
+				Coords: primitives.Coords{
+					X: x,
+					Y: y,
+				},
+				CellType: contracts.EmptyCell,
+				Cost:     0,
+			}
+			terrain.Cells = append(terrain.Cells, &currentCell)
+		}
+	}
+
+	terrain.Cells[21].SetCellType(contracts.OrganicCell)
+	terrain.Cells[21].SetCost(4)
+
+	terrain.Cells[23].SetCellType(contracts.OrganicCell)
+	terrain.Cells[23].SetCost(4)
+
+	terrain.Cells[43].SetCellType(contracts.OrganicCell)
+	terrain.Cells[43].SetCost(4)
+
+	terrain.Cells[45].SetCellType(contracts.OrganicCell)
+	terrain.Cells[45].SetCost(4)
+
+	terrain.Cells[15].SetCellType(contracts.OrganicCell)
+	terrain.Cells[15].SetCost(4)
+
+	terrain.Cells[17].SetCellType(contracts.OrganicCell)
+	terrain.Cells[17].SetCost(4)
+
+	terrain.Cells[37].SetCellType(contracts.OrganicCell)
+	terrain.Cells[37].SetCost(4)
+
+	terrain.Cells[39].SetCellType(contracts.OrganicCell)
+	terrain.Cells[39].SetCost(4)
+
+	terrain.Cells[9].SetCellType(contracts.OrganicCell)
+	terrain.Cells[9].SetCost(4)
+
+	return &terrain
+}
+
+func GetAgent() contracts.IAgent {
+	agent := &agents.Agent{
+		IBrain: &core.Brain{
+			CommandList: commands.CommandList{
+				Commands: GetCommands(),
+			},
+			Commands: []int{
+				0, 4, //down
+				1, 4, //eat down
+				14,
+				11,
+				2,
+				1,
+				0, 2, //right
+				1, 2, //eat right
+				14,
+				11,
+				2,
+				1,
+			},
+			CurrentAddress: 0,
+		},
+		Coords: primitives.Coords{
+			X: 1,
+			Y: 0,
+		},
+		IReproduction: &reproductions.BuddingReproduction{},
+		Energy:        22,
+		Generation:    0,
+	}
+	return agent
+}
+
 func TestWorld_Action_SpecificAgent(t *testing.T) {
 
 	rand.Seed(time.Now().UnixNano())
 
-	commandsMap := make(map[int]contracts.ICommand)
-	commandsMap[0] = &preparedCommands.MoveCommand{
-		commands.Command{
-			IsInterrupt: true,
-		},
-	}
-	commandsMap[1] = &preparedCommands.EatCommand{
-		commands.Command{
-			IsInterrupt: false,
-		},
-	}
+	agent := GetAgent()
 
-	agent := agents.Agent{
-		IBrain: &core.Brain{
-			CommandList: commands.CommandList{
-				Commands: commandsMap,
-			},
-			Commands: []int{
-				0, 4, //down
-				1, 4, //eat down
-				14,
-				11,
-				2,
-				1,
-				0, 2, //right
-				1, 2, //eat right
-				14,
-				11,
-				2,
-				1,
-			},
-			CurrentAddress: 0,
-		},
-		Coords: primitives.Coords{
-			X: 1,
-			Y: 0,
-		},
-		IReproduction: &reproductions.BuddingReproduction{
-			ReproductionPower: 50,
-		},
-		Energy:     22,
-		Generation: 0,
-	}
-
-	terrain := terrains.MooreTerrain{
-		terrains.Terrain{
-			Cells:  make([]contracts.ICell, 0),
-			Width:  10,
-			Height: 5,
-		},
-	}
-
-	for y := 0; y < terrain.Height; y++ {
-		for x := 0; x < terrain.Width; x++ {
-			currentCell := terrains.Cell{
-				Coords: primitives.Coords{
-					X: x,
-					Y: y,
-				},
-				CellType: contracts.EmptyCell,
-				Cost:     0,
-			}
-			terrain.Cells = append(terrain.Cells, &currentCell)
-		}
-	}
-
-	terrain.Cells[1].SetCellType(contracts.LockedCell)
-	terrain.Cells[1].SetAgent(&agent)
-
-	terrain.Cells[21].SetCellType(contracts.OrganicCell)
-	terrain.Cells[21].SetCost(3)
-
-	terrain.Cells[23].SetCellType(contracts.OrganicCell)
-	terrain.Cells[23].SetCost(3)
-
-	terrain.Cells[43].SetCellType(contracts.OrganicCell)
-	terrain.Cells[43].SetCost(3)
-
-	terrain.Cells[45].SetCellType(contracts.OrganicCell)
-	terrain.Cells[45].SetCost(3)
-
-	terrain.Cells[15].SetCellType(contracts.OrganicCell)
-	terrain.Cells[15].SetCost(3)
-
-	terrain.Cells[17].SetCellType(contracts.OrganicCell)
-	terrain.Cells[17].SetCost(3)
-
-	terrain.Cells[37].SetCellType(contracts.OrganicCell)
-	terrain.Cells[37].SetCost(3)
-
-	terrain.Cells[39].SetCellType(contracts.OrganicCell)
-	terrain.Cells[39].SetCost(3)
-
-	terrain.Cells[9].SetCellType(contracts.OrganicCell)
-	terrain.Cells[9].SetCost(3)
+	terrain := GetTerrain()
+	terrain.GetCell(1, 0).SetCellType(contracts.LockedCell)
+	terrain.GetCell(1, 0).SetAgent(agent)
 
 	world := World{
-		&terrain,
+		terrain,
 	}
-
 	world.Action(1, gui.DrawFrame)
 
 }
 
-func TestWorld_Action_SpecificAgentWithChild(t *testing.T) {
-
+func BenchmarkWorld_Action_SpecificAgent_WithDraw(b *testing.B) {
 	rand.Seed(time.Now().UnixNano())
-	commandsMap := make(map[int]contracts.ICommand)
-	commandsMap[0] = &preparedCommands.MoveCommand{
-		commands.Command{
-			IsInterrupt: true,
-		},
-	}
-	commandsMap[1] = &preparedCommands.EatCommand{
-		commands.Command{
-			IsInterrupt: false,
-		},
-	}
 
-	agent := agents.Agent{
-		IBrain: &core.Brain{
-			CommandList: commands.CommandList{
-				Commands: commandsMap,
-			},
-			Commands: []int{
-				0, 4, //down
-				1, 4, //eat down
-				14,
-				11,
-				2,
-				1,
-				0, 2, //right
-				1, 2, //eat right
-				14,
-				11,
-				2,
-				1,
-			},
-			CurrentAddress: 0,
-		},
-		Coords: primitives.Coords{
-			X: 1,
-			Y: 0,
-		},
-		IReproduction: &reproductions.BuddingReproduction{
-			ReproductionPower:   25,
-			MutationProbability: 100,
-		},
-		Energy:     30,
-		Generation: 0,
-	}
+	agent := GetAgent()
 
-	terrain := terrains.MooreTerrain{
-		terrains.Terrain{
-			Cells:  make([]contracts.ICell, 0),
-			Width:  10,
-			Height: 5,
-		},
-	}
-
-	for y := 0; y < terrain.Height; y++ {
-		for x := 0; x < terrain.Width; x++ {
-			currentCell := terrains.Cell{
-				Coords: primitives.Coords{
-					X: x,
-					Y: y,
-				},
-				CellType: contracts.EmptyCell,
-				Cost:     0,
-			}
-			terrain.Cells = append(terrain.Cells, &currentCell)
-		}
-	}
-
-	terrain.Cells[1].SetCellType(contracts.LockedCell)
-	terrain.Cells[1].SetAgent(&agent)
-
-	terrain.Cells[21].SetCellType(contracts.OrganicCell)
-	terrain.Cells[21].SetCost(4)
-
-	terrain.Cells[23].SetCellType(contracts.OrganicCell)
-	terrain.Cells[23].SetCost(4)
-
-	terrain.Cells[43].SetCellType(contracts.OrganicCell)
-	terrain.Cells[43].SetCost(4)
-
-	terrain.Cells[45].SetCellType(contracts.OrganicCell)
-	terrain.Cells[45].SetCost(4)
-
-	terrain.Cells[15].SetCellType(contracts.OrganicCell)
-	terrain.Cells[15].SetCost(4)
-
-	terrain.Cells[17].SetCellType(contracts.OrganicCell)
-	terrain.Cells[17].SetCost(4)
-
-	terrain.Cells[37].SetCellType(contracts.OrganicCell)
-	terrain.Cells[37].SetCost(4)
-
-	terrain.Cells[39].SetCellType(contracts.OrganicCell)
-	terrain.Cells[39].SetCost(4)
-
-	terrain.Cells[9].SetCellType(contracts.OrganicCell)
-	terrain.Cells[9].SetCost(4)
+	terrain := GetTerrain()
+	terrain.GetCell(1, 0).SetCellType(contracts.LockedCell)
+	terrain.GetCell(1, 0).SetAgent(agent)
 
 	world := World{
-		&terrain,
+		terrain,
 	}
-
 	world.Action(1, gui.DrawFrame)
-
 }
 
-func TestWorld2(t *testing.T) {
+func BenchmarkWorld_Action_SpecificAgent_WithoutDraw(b *testing.B) {
 	rand.Seed(time.Now().UnixNano())
-	commandsMap := make(map[int]contracts.ICommand)
-	commandsMap[0] = &preparedCommands.MoveCommand{
-		commands.Command{
-			IsInterrupt: true,
-		},
-	}
-	commandsMap[1] = &preparedCommands.EatCommand{
-		commands.Command{
-			IsInterrupt: false,
-		},
-	}
 
-	agent := agents.Agent{
-		IBrain: &core.Brain{
-			CommandList: commands.CommandList{
-				Commands: commandsMap,
-			},
-			Commands: []int{
-				0, 4, //down
-				1, 4, //eat down
-				14,
-				11,
-				2,
-				1,
-				0, 2, //right
-				1, 2, //eat right
-				14,
-				11,
-				2,
-				1,
-			},
-			CurrentAddress: 0,
-		},
-		Coords: primitives.Coords{
-			X: 1,
-			Y: 0,
-		},
-		IReproduction: &reproductions.MitosisReproduction{
-			ReproductionPower:   1000,
-			MutationProbability: 100,
-			GenerationPower:     2,
-		},
-		Energy:     22,
-		Generation: 0,
-	}
+	agent := GetAgent()
 
-	terrain := terrains.MooreTerrain{
-		terrains.Terrain{
-			Cells:  make([]contracts.ICell, 0),
-			Width:  10,
-			Height: 5,
-		},
-	}
-
-	for y := 0; y < terrain.Height; y++ {
-		for x := 0; x < terrain.Width; x++ {
-			currentCell := terrains.Cell{
-				Coords: primitives.Coords{
-					X: x,
-					Y: y,
-				},
-				CellType: contracts.EmptyCell,
-				Cost:     0,
-			}
-			terrain.Cells = append(terrain.Cells, &currentCell)
-		}
-	}
-
-	terrain.Cells[1].SetCellType(contracts.LockedCell)
-	terrain.Cells[1].SetAgent(&agent)
-
-	terrain.Cells[21].SetCellType(contracts.OrganicCell)
-	terrain.Cells[21].SetCost(4)
-
-	terrain.Cells[23].SetCellType(contracts.OrganicCell)
-	terrain.Cells[23].SetCost(4)
-
-	terrain.Cells[43].SetCellType(contracts.OrganicCell)
-	terrain.Cells[43].SetCost(4)
-
-	terrain.Cells[45].SetCellType(contracts.OrganicCell)
-	terrain.Cells[45].SetCost(4)
-
-	terrain.Cells[15].SetCellType(contracts.OrganicCell)
-	terrain.Cells[15].SetCost(4)
-
-	terrain.Cells[17].SetCellType(contracts.OrganicCell)
-	terrain.Cells[17].SetCost(4)
-
-	terrain.Cells[37].SetCellType(contracts.OrganicCell)
-	terrain.Cells[37].SetCost(4)
-
-	terrain.Cells[39].SetCellType(contracts.OrganicCell)
-	terrain.Cells[39].SetCost(4)
-
-	terrain.Cells[9].SetCellType(contracts.OrganicCell)
-	terrain.Cells[9].SetCost(4)
+	terrain := GetTerrain()
+	terrain.GetCell(1, 0).SetCellType(contracts.LockedCell)
+	terrain.GetCell(1, 0).SetAgent(agent)
 
 	world := World{
-		&terrain,
+		terrain,
 	}
+	world.Action(1, func(terrain contracts.ITerrain, i int) {
 
-	world.Action(1, gui.DrawFrame)
+	})
 }
